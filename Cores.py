f1631c25c18cf70fc3f9d01ccf246440ea067a94
eldercamposds <eldercamposdev@gmail.com>
2023-10-16 22:40:04 -0300

    desenhando comida e snake

--- Cores.py
+++ Cores.py
@@ -1,11 +1,25 @@
-class Cores:
-    def cores(self, preta, branca, vermelha, verde):
-        self._preta = (0, 0, 0)
-        self._branca = (255, 255, 255)
-        self._vermelha = (255, 0, 0)
-        self._verde = (0, 255, 0)
-
+class Cores():
+    def __init__(self, cor):
+        self._cor = cor
+    
     @property
-    def preta(self):
-        return self._preta
-        
+    def cor(self):
+        return self._cor
+    
+    @cor.setter
+    def cor(self, cor):
+        if (cor == "preta".strip().split()):
+            self._cor = (0,0,0)
+        if (cor == "verde"):
+            self._cor = (0, 255, 0)
+
+# teste = Cores.cor.setter("verde")
+
+# print(teste)
+     
+        # self._branca = (255, 255, 255)
+        # self._vermelha = (255, 0, 0)
+        # self._verde = (0, 255, 0)
+
+
+
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import Cores
+from Cores import *
 
 pygame.init()# inicializar o pygame
 pygame.display.set_caption("Snake Python")
@@ -9,21 +9,51 @@ tela = pygame.display.set_mode((largura, altura))
 relogio = pygame.time.Clock()
 
 #cores
-black = Cores
+preta =  (0, 0, 0)
+branca = (255, 255, 255)
+svermelha = (255, 0, 0)
+verde = (0, 255, 0)
 
 #parametros cobrinha
 tamanho_quadrado = 10
-velocidade_cobra = 15
+velocidade_atualizacao = 15
 
-#loop
+def gerar_comida():#gerar posição das comidas
+    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / tamanho_quadrado) * tamanho_quadrado
+    comida_y = round(random.randrange(0, altura- tamanho_quadrado)/ tamanho_quadrado) * tamanho_quadrado
+    return comida_x, comida_y
+
+def desenha_comida(tamanho, comida_x, comida_y): #desenhar comidas na tela
+    pygame.draw.rect(tela, verde, [comida_x, comida_y, tamanho, tamanho])
+
 def rodar_jogo():
     fim_jogo = False
 
+    x = largura / 2 
+    y = altura / 2
+
+    velocidade_x = 0
+    velocidade_y = 0
+
+    tamanho_cobra = 1
+    pixel = []
+
+    comida_x , comida_y = gerar_comida()
+    
+
     while not fim_jogo:
 
-        tela.fill(black)
+        tela.fill(preta)
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 fim_jogo = True
-        
+                
+        # desenha comida
+        desenha_comida(tamanho_quadrado, comida_x, comida_y)
+
+        #atualização da tela
+        pygame.display.update()
+        relogio.tick(velocidade_atualizacao)
+
+rodar_jogo()
